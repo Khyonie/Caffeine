8dd182d7d5ece556ed3c10bcd3bc8004714fcf21
Khyonie <khyonie@proton.me>
2025-12-29 21:05:15 -0700

    Caffeine command

--- src/plugin/coffee/khyonieheart/caffeine/Caffeine.java
+++ src/plugin/coffee/khyonieheart/caffeine/Caffeine.java
@@ -1,6 +1,8 @@
 package coffee.khyonieheart.caffeine;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -10,13 +12,15 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import coffee.khyonieheart.anenome.operation.Results;
 import coffee.khyonieheart.lilac.LilacDecoder;
+import coffee.khyonieheart.lilac.LilacEncoder;
+import coffee.khyonieheart.lilac.TomlEncoder;
 import coffee.khyonieheart.lilac.TomlVersion;
 
 public class Caffeine extends JavaPlugin
 {
 	private static Map<UUID, PlayerData> loadedPlayerData = new HashMap<>();
-	
 	public static final String DATA_FOLDER_PATH = "./Caffeine/";
+	public static final String PLUGIN_VERSION = "0.1.0";
 
 	@Override
 	public void onEnable()
@@ -27,7 +31,17 @@ public class Caffeine extends JavaPlugin
 	@Override 
 	public void onDisable()
 	{
-
+		TomlEncoder encoder = new LilacEncoder();
+		loadedPlayerData.forEach((uuid, data) -> {
+			File file = new File(DATA_FOLDER_PATH + uuid.toString() + ".toml");
+			String toml = encoder.encode(data.toMap());
+			
+			try {
+				Files.writeString(file.toPath(), toml);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		});
 	}
 
 	public static PlayerData loadPlayerData(
